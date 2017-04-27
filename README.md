67d45769372fbf536111c0453f76fe73b95baa4e
Ben Wilcox <reccanti@gmail.com>
2017-04-26 21:50:56 -0400

    Refactor for testing purposes
    Refactors the database and env files for testing purposes. Also adds
    a badge to the README

---
+++ README.md
@@ -0,0 +1,1 @@
+[![CircleCI](https://circleci.com/gh/reccanti/StudentRecords.svg?style=svg)](https://circleci.com/gh/reccanti/StudentRecords)
--- src/database.ts
+++ src/database.ts
@@ -1,19 +1,24 @@
 import * as knex from 'knex';
 
-/**
- * Initialize our knex. This can be used reused
- * throughout the app
- */
-const client = knex({
-    dialect: 'mariadb',
-    connection: {        
-        host: process.env['DB_HOST'],
-        user: process.env['DB_USER'],
-        password: process.env['DB_PASSWORD'],
-        db: process.env['DB_DATABASE'],
-        port: process.env['DB_PORT'],
-        charset: 'utf8'
-    }
-});
+let client;
+if (process.env['NODE_ENV'] != 'TEST') {
+    /**
+     * Initialize our knex. This can be used reused
+     * throughout the app
+     */
+    client = knex({
+        dialect: 'mariadb',
+        connection: {        
+            host: process.env['DB_HOST'],
+            user: process.env['DB_USER'],
+            password: process.env['DB_PASSWORD'],
+            db: process.env['DB_DATABASE'],
+            port: process.env['DB_PORT'],
+            charset: 'utf8'
+        }
+    });
+} else {
+    client = {};
+}
 
 export default client;
